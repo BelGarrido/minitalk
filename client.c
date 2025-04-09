38beed9ba2efe190e46a950a858cbd6489ae0f7f
Ana <anaigd93@gmail.com>
2025-04-09 11:29:31 +0200

    minitalk working3

--- client.c
+++ client.c
@@ -1,47 +1,38 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   client.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: anagarri@student.42malaga.com <anagarri    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/04 13:32:42 by anagarri@st       #+#    #+#             */
-/*   Updated: 2025/04/04 14:07:23 by anagarri@st      ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 // client takes 2 arguments (server PID, the string to communicate)
 // must sent the string passed as argument to the server
+
+
 /*char_to_bin
  1 iterar sobre elstring
  2 char_to_bin devuelve binario almacenarlo en un int o en un cha
- 3 bucle iterando sobre el int para seleccionar cada valor y mandar
- una señal u otra.
+ 3 bucle iterando sobre el int para seleccionar cada valor y mandar una señal u otra.
  */
+
 
 #include "minitalk.h"
 
-void	signal_handler(int signum)
+void signal_handler(int signum)
 {
 	printf("Server confirmation recived\n");
 }
 
-int	main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	char				*s;
-	int					pid;
-	char				*binary;
-	int					i;
-	struct sigaction	csa;
+	char *s;
+	int pid;
+	char *binary;
+	char *binary_free;
+	int i;
+	struct sigaction csa;
 
 	if (argc != 3)
 	{
-		printf("Error: 2 arguments needed (process ID and a string)\n");
-		return (1);
+		printf("Error: You have to pass 2 arguments (process ID and a string)\n");
+		return 1;
 	}
 	csa.sa_handler = signal_handler;
-	sigemptyset(&csa.sa_mask);
-	csa.sa_flags = 0;
+	sigemptyset(&csa.sa_mask); // Not blocking any other signals
+	csa.sa_flags = 0; //Use siginfo_t to get sender's PID
 	sigaction(SIGUSR1, &csa, NULL);
 	s = argv[2];
 	pid = atoi(argv[1]);
@@ -49,21 +40,27 @@ int	main(int argc, char *argv[])
 	while (s[i] != '\0')
 	{
 		binary = char_to_binary(s[i]);
+		binary_free = binary;
 		i++;
 		while (*binary != '\0')
 		{
 			if (*binary == '0')
 			{
 				kill(pid, SIGUSR2);
-				sleep(1);
+				usleep(50);
 			}
 			else if (*binary == '1')
-			{
+			{	
 				kill(pid, SIGUSR1);
-				sleep(1);
+				usleep(50);
 			}
 			binary++;
 		}
+		free(binary_free);
 	}
-	return (0);
 }
+
+/* void send_signal(pid_t pid, char *s)
+{
+
+} */
--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: anagarri@student.42malaga.com <anagarri    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 16:19:06 by anagarri          #+#    #+#             */
-/*   Updated: 2025/04/04 13:56:22 by anagarri@st      ###   ########.fr       */
+/*   Updated: 2025/04/05 12:20:42 by anagarri@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,15 @@
 # include <stdint.h>
 # include <signal.h>
 
+extern int str_len;
+
 //initialized the binary string to '0'
 void	*ft_memset(void *ptr, int x, size_t n);
 char	*char_to_binary(unsigned char c);
 
 #endif
+
+
 // LIST OF THINGS TO CHANCE LATER
 
 	//PRINTF -->FT_PRINTF
